3dedf4dab033a39945f9c905971b669a1973a519
Pratheeban <epratheeban@gmail.com>
2022-03-28 23:46:05 +0200

    sds

--- 2_download_requirements.sh
+++ 2_download_requirements.sh
@@ -1,7 +1,6 @@
 sudo apt install -y python3-pip
 sudo -H pip3 install -U jetson-stats
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.t$
-
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=19yXDpF90Lr4J23cdGCVHPeFSyGCVR3VN' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=19yXDpF90Lr4J23cdGCVHPeFSyGCVR3VN" -O setup_jetson.zip && rm -rf /tmp/cookies.txt
 unzip setup_jetson.zip
 cd setup_jetson
 chmod u+x opencv-install.sh
@@ -10,6 +9,5 @@ cd ..
 
 mkdir Datasets && cd Datasets
 mkdir Kapsch && cd Kapsch
-#gdown 1qQzjBgBpnorkrsyG0ex-bAsl5q513tJI
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.t$
+
 cd
