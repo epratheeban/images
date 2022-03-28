3ea82419149ad3b58c2ea0a284d6649d7377f190
Pratheeban <epratheeban@gmail.com>
2022-03-28 23:43:52 +0200

    update

--- 2_download_requirements.sh
+++ 2_download_requirements.sh
@@ -1,7 +1,7 @@
 sudo apt install -y python3-pip
 sudo -H pip3 install -U jetson-stats
-pip3 install gdown
-gdown https://drive.google.com/uc?id=19yXDpF90Lr4J23cdGCVHPeFSyGCVR3VN
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.t$
+
 unzip setup_jetson.zip
 cd setup_jetson
 chmod u+x opencv-install.sh
@@ -10,6 +10,6 @@ cd ..
 
 mkdir Datasets && cd Datasets
 mkdir Kapsch && cd Kapsch
-gdown 1qQzjBgBpnorkrsyG0ex-bAsl5q513tJI
-cd 
-
+#gdown 1qQzjBgBpnorkrsyG0ex-bAsl5q513tJI
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.t$
+cd
